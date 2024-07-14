fe0dca74e539795f686bad53de8309fba4723c10
NischalGiriyan <nischal.giriyan@gmail.com>
2024-07-14 20:49:55 +0200

    Update xgboost_labelled.py

--- Anomaly_Detection/xgboost_labelled.py
+++ Anomaly_Detection/xgboost_labelled.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
-# Load the dataset
+# Load the CSv file
 df = pd.read_csv('feature_list.csv')
 
 # Separate features (X) and labels (y)
 X = df.drop(columns=['file', 'type'])
 y = df.iloc[:, -1]   # Last column (labels)
 
-# Convert labels to binary format if necessary ('OK' -> 0, 'Anomaly' -> 1)
+# Convert labels to binary format('OK' -> 0, 'Anomaly' -> 1)
 y = y.apply(lambda x: 0 if x == 'OK' else 1)
 
 # Split the data into training and testing sets
@@ -18,14 +18,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Initialize XGBoost classifier
 model = XGBClassifier(
-    objective='binary:logistic',   # Binary classification with logistic regression
-    eval_metric='logloss',         # Metric to evaluate during training
-    learning_rate=0.1,             # Learning rate (default is 0.3)
-    n_estimators=100,              # Number of boosting rounds (default is 100)
-    max_depth=15,                   # Maximum depth of trees (default is 6)
-    subsample=0.8,                 # Fraction of samples used for training each tree (default is 1.0)
-    colsample_bytree=0.9,          # Fraction of features used for training each tree (default is 1.0)
-    random_state=42                # Random state for reproducibility
+    objective='binary:logistic',  
+    eval_metric='logloss',         
+    learning_rate=0.1,            
+    n_estimators=100,              
+    max_depth=15,                  
+    subsample=0.8,                 
+    colsample_bytree=0.9,          
+    random_state=42                
 )
 
 # Train the model
